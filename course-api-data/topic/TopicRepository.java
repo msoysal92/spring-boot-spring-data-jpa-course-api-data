605cbe677479192a8341e08efb3c842f7068e3c7
msoys <msoys@192.168.1.10>
2017-12-08 19:21:34 +0300

    Fix packaging

--- course-api-data/topic/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package com.soysal.springbootquickstart.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String>{
-
-
-}
